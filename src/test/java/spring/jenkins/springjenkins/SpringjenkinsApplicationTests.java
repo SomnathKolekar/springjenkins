abdfc47e35ef1e545ea654d752fb5f3b1581b066
Somnath Kolekar <somnathkolekar023@gmail.com>
2022-11-02 19:39:03 +0530

    first commit

---
+++ src/test/java/spring/jenkins/springjenkins/SpringjenkinsApplicationTests.java
@@ -0,0 +1,20 @@
+package spring.jenkins.springjenkins;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjenkinsApplicationTests {
+	static Logger logger = LoggerFactory.getLogger(SpringjenkinsApplication.class);
+
+	@Test
+	void contextLoads() {
+		logger.info("test case executing started...");
+		assertEquals(true, true);
+	}
+
+}
